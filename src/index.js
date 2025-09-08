7f349d965a2d69e9aba3cf026c21ed28a7271068
ashish-leo <ashishleo12@gmail.com>
2025-09-08 10:11:46 +0530

    cron.js added

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import authRoutes from "./routes/authRoutes.js";
 import bookRoutes from "./routes/bookRoutes.js";
 import { dbConnect } from "./lib/db.js";
 import cors from "cors";
+import job from "./lib/cron.js";
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cors());
+
+job.start();
 
 app.use("/api/auth", authRoutes);
 app.use("/api/books", bookRoutes);
---
+++ src/lib/cron.js
@@ -0,0 +1,13 @@
+import cron from "cron";
+import https from "https";
+
+const job = new cron.CronJob("*/14 * * * *", function () {
+  https
+    .get("https://react-native-book-app-backend.onrender.com", (res) => {
+      if (res.statusCode === 200) console.log("Get request sent successfully");
+      else console.log("Get request failed", res.statusCode);
+    })
+    .on("error", (e) => console.log("Error while sending req"));
+});
+
+export default job;
